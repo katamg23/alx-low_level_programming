1f6ff0492823666b499b6795a1b7ec0bbdc6fb02
Katam G <jepletingg23@gmail.com>
2022-10-16 16:12:15 -0700

    first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+
+
+
+
+/**
+ *
+ *  * 
+ *
+ *   *  * main - Entry point
+ *
+ *    * 
+ *
+ *     *  *
+ *
+ *      * 
+ *
+ *       *   * Return: Always 0 (Success)
+ *
+ *        * 
+ *
+ *         *   */
+
+
+
+int mmain(void)
+
+
+
+{
+
+
+
+	                puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			                     return (0);
+
+
+
+
+
+
+
+}
+
+a
+
+
+
+
